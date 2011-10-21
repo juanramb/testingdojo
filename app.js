d82f53dfee4223f03a3efe675b3c147e65d81758
juanra <jrmartin@visual-trader.com>
2011-10-21 17:14:30 +0200

    sync

--- app.js
+++ app.js
@@ -8,6 +8,38 @@ var express = require('express');
 var app = module.exports = express.createServer();
 
 var io = require('socket.io').listen(app);
+// MongoDB
+
+var mongoose = require("mongoose"),
+    Schema = mongoose.Schema,
+    ObjectId = Schema.ObjectId,
+    DocumentObjectId = mongoose.Types.ObjectId;
+//Mongo connection
+var db = mongoose.createConnection("mongo://localhost/ticker-plant");
+
+//Schemas
+var Exchange = new Schema({
+  exchange: { type: String}
+});
+mongoose.model('exchanges',Exchange);
+var Exchanges = db.model('exchanges', Exchange);
+var Group = new Schema({
+  name: { type: String},
+  ticker: { type: String},
+  exchange: { type: String}
+});
+mongoose.model('groups', Group);
+var Security = new Schema({
+  ticker: { type: String},
+  exchange: {type: String},
+  currency: {type: String}
+});
+mongoose.model('securities',Security);
+
+
+
+// Redis
+var redis = require("redis");
 // Configuration
 
 app.configure(function(){
@@ -32,6 +64,34 @@ app.configure('production', function(){
 app.get('/', function(req, res){
   res.render('index', {
     title: 'Express'
+  });
+});
+//get exchanges
+app.get('/exchanges', function(req,res){
+  Exchanges.find({}, function(err, data){
+    if(err) {
+      throw(err);
+    }
+     exchanges=JSON.parse(data);
+     res.send(exchanges); 
+  });
+});
+//get groups
+app.get('/exchange/:exchange/', function(req,res){
+  Group.findByExchange(req.params.exchange, function(err, data){
+    if(err) {
+      throw(err);
+    }
+     res.send(JSON.stringify(data)); 
+  });
+});
+//get group tickers
+app.get('/exchange/:exchange/:group', function(req,res){
+  Group.findByName(req.params.group, function(err, data){
+    if(err) {
+      throw(err);
+    }
+     res.send(JSON.stringify(data)); 
   });
 });
 
--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,6 +1,54 @@
+
 dojo.require("dojo.fx");
+dojo.addOnLoad(function(){
+	var socket = new io.Socket();
+	//get simbols from mongo
+
+	//draw table based on symbols
+	
+	socket.on("message", function(msg) {
+		var data = dojo.fromJson(msg);
+		var html = dojo.byId("tick-table").innerHTML;
+		dojo.byId("ticker-symbols").innerHTML = "<div class='ticker-data'>" +
+		"<div class='ticker-symbol'><a class='ticker-symb-link' href='#' data='" + data.symbol + "' title='Ticker Symbol Analysis'>" + data.symbol + "</a></div>" +
+		"<div class='ticker-meta ticker-price'>Price: $" + data.price + "</div>" +
+		"<div class='ticker-meta ticker-volume'>Volume: " + data.volume + "</div>" +
+		"</div>" + html;
+		
+		dojo.query(".ticker-symb-link").onclick(handleSymbolClick);
+		
+		var nodes = dojo.query("#ticker-symbols .ticker-data")
+		var total = 0;
+		var d = dojo;
+		var lastWidth = 0;
+		
+		nodes.forEach(function(node, index, nodes) {
+		var style = d.getComputedStyle(node);
+		var width = parseInt(style.width.replace(/px/, ""));
+		total += width;
+		lastWidth = width;
+		});
+		total += lastWidth;
+		
+		var newRight = (lastWidth * -1);
+		dojo.style("ticker-symbols", {
+		width: (total + (total/2)) + "px",
+		right: newRight + "px"
+		});
+		dojo.anim("ticker-symbols", {
+		right: {
+		start: newRight,
+		end: 16,
+		unit: "px"
+		}
+	});
+});
+
+
+});
 dojo.ready(function(){
   dojo.byId("hola").innerHTML += ", desde la "+ dojo.version;
+  
   dojo.fx.slideTo({
 	  top: 100,
 	  left: 200,
